eb068850b6ddb58b0e59b5f703baf7bc656a3b43
ABI <alain@top.fr>
2019-12-13 01:09:36 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Cartes;
+using Jeu;
+using Joueurs;
+using Paquets;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("bonjour bonjour");
+            Console.ReadKey();
+            /*
+            MemoryDessin partie = new MemoryDessin(4);
+            partie.Add("Bernard");
+            partie.Add("Michel");
+            partie.Jouer();
+            */
+            /*
+            Memory32 partie = new Memory32();
+            partie.Add("Bernard");
+            partie.Jouer();*/
+            /*
+            PlusOuMoins partie = new PlusOuMoins();
+            partie.Jouer();*/
+        }
+    }
+}
